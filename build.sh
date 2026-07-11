2c9a3700659c6bdbae6ec3ae6166de2a16ddde20
francistheodorecatte <perturbed.old.cat@gmail.com>
2026-07-11 19:54:50 -0400

    build.sh cleanup on non-clean exit & check for sdcc

--- build.sh
+++ build.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 set -euo pipefail
+trap "cleanup" EXIT
+
+cleanup() {
+	cd ..
+	rm -r build
+}
 
 mkdir build && cd build
+
+if ! [ -x "$(command -v sdcc)" ]; then
+	echo "Build script requires sdcc!" >&2
+	exit 1
+fi
+
 sdcc -mmcs51 -c ../src/Delay.c -D FOSC_160000 -I../include
 sdcc -mmcs51 -c ../src/Common.c -D FOSC_160000 -I../include
 sdcc -mmcs51 -c ../src/main.c -D FOSC_160000 -I../include
-sdcc -mmcs51 -o firmware.hex main.rel Delay.rel Common.rel -D FOSC_160000 -I../include
-cp firmware.hex ../ && cd .. && rm -r build
+sdcc -mmcs51 -o firmware.hex main.rel Delay.rel Common.rel
+cp firmware.hex ../
+
+exit 0
 
