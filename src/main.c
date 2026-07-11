af57a6ae840b05103c9b3ef20c860be31edebda5
francistheodorecatte <perturbed.old.cat@gmail.com>
2026-07-10 20:56:55 -0400

    Update fw for single relay board, add relay PWM feature to reduce power consumption.

--- src/main.c
+++ src/main.c
@@ -3,21 +3,21 @@
 #include <nuvoton/Common.h>
 #include <nuvoton/Delay.h>
 #include <nuvoton/SFR_Macro.h>
-#define RELAY4 P01
-#define RELAY3 P04
-#define RELAY2 P03
-#define RELAY1 P05
 
-#define RED P11
-#define GREEN P00
-#define BLUE P10
+#define BIT_TMP
 
-#define PWM_GREEN PWM3L
+#define RELAY P15
+
+#define RED P02
+#define GREEN P03
+#define BLUE P04
+
+#define PWM_RELAY PWM5L
 #define PWM_BLUE PWM2L
-#define PWM_RED PWM1L
 
-#define BUTTON1 P15
-#define BUTTON2 P12
+
+#define BUTTON1 P01
+#define BUTTON2 P00
 
 int getchar(void)
 {
@@ -40,15 +40,8 @@ int putchar (int c)
 
 /**
  *  Stock protocol
- *  Open relay  1:A0 01 01 A2
- *  Close relay 1:A0 01 00 A1
- *  Open relay  2:A0 02 01 A3
- *  Close relay 2:A0 02 00 A2
- *  Open relay  3:A0 03 01 A4
- *  Close relay 3:A0 03 00 A3
- *  Open relay  4:A0 04 01 A5
- *  Close relay 4:A0 04 00 A4 
- * 
+ *  Open relay:A0 01 01 A2
+ *  Close relay:A0 01 00 A1
  */
 
 void receive_packet(unsigned char *tmp)
@@ -71,29 +64,23 @@ void receive_packet(unsigned char *tmp)
 void uart_loop() {
 	unsigned char tmp[4];
 	receive_packet(tmp);
+	BLUE = 1;
 	switch(tmp[1]) {
 		case 1:
-			RELAY1 = tmp[2] ? 1 : 0;
-			break;
-		case 2:
-			RELAY2 = tmp[2] ? 1 : 0;
-			break;
-		case 3:
-			RELAY3 = tmp[2] ? 1 : 0;
-			break;
-		case 4:
-			RELAY4 = tmp[2] ? 1 : 0;
+			RELAY = tmp[2] ? 255 : 0; //100% PWM if payload is not 0
+			if ( RELAY > 1) {
+				Timer0_Delay1ms(100); //wait 100ms for relay to stabilize
+				RELAY = 130; //51% PWM to reduce relay current draw
+			}
 			break;
 		case 0x11:
-			PWM_RED = tmp[2]; 
-			LOAD=1;
+			RED = tmp[2] ? 1 : 0;
 			break;
 		case 0x12:
-			PWM_GREEN = tmp[2]; 
-			LOAD=1;
+			GREEN = tmp[2] ? 1: 0;
 			break;
 		case 0x13:
-			PWM_BLUE = tmp[2]; 
+			PWM_BLUE = tmp[2];
 			LOAD=1;
 			break;
 		/* Pollin' ain't a good idea here */
@@ -108,7 +95,7 @@ void uart_loop() {
 
 void uart_init(UINT32 u32Baudrate) //T1M = 1, SMOD = 1
 {
-	P06_PushPull_Mode; 
+	P06_PushPull_Mode;
 	P07_Input_Mode;
 
 	SCON = 0x50;  //UART0 Mode1,REN=1,TI=1
@@ -138,21 +125,17 @@ int main()
 {
 	uart_init(19200);
 	printf("\n\n\nHello world. I'm a hacky opensource firmware for LC-tech modules\n");
-	printf("Go check out my blog https://ncrmnt.org for more awesome stuff\n");
-	/* Relays */
-	P01_PushPull_Mode;
-	P03_PushPull_Mode;
-	P04_PushPull_Mode;
-	P05_PushPull_Mode;
+	/* Relay */
+	P15_PushPull_Mode;
 
 	/* LEDS */
-	P10_PushPull_Mode;
-	P11_PushPull_Mode;
-	P00_PushPull_Mode;
+	P02_PushPull_Mode;
+	P03_PushPull_Mode;
+	P04_PushPull_Mode;
 
 	/* Buttons */
-	P12_Input_Mode;
-	P15_Input_Mode;
+	P01_Input_Mode;
+	P00_Input_Mode;
 
 	/* buttonz */
 	PWMPH = 0;
@@ -160,20 +143,18 @@ int main()
 	PWM_CLOCK_FSYS;
 	PWM_CLOCK_DIV_128;
 	PWM_EDGE_TYPE;
-	PWM1_P11_OUTPUT_ENABLE;
-	PWM2_P10_OUTPUT_ENABLE;
-	PWM3_P00_OUTPUT_ENABLE;
+	PWM3_P04_OUTPUT_ENABLE;
+	PWM5_P15_OUTPUT_ENABLE;
 
 	RED = 0;
 	GREEN = 0;
-	BLUE = 1;
+	BLUE = 0;
 
 	Timer0_Delay1ms(500);
-	PWM1H = 0;
-	PWM2H = 0;
 	PWM3H = 0;
+	PWM5H = 0;
 
-	PWM_RED = 0x10;
+	RED = 1;
 	PWMRUN = 1;
 	while (1)
 	{
